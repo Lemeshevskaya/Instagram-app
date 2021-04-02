c2cab07838b0561a7ff04b76b35bd1075729e1fc
Gulshan-p <78453319+Gulshan-p@users.noreply.github.com>
2021-04-01 17:57:40 -0700

    followers and following route with Schema

--- models/Profile.js
+++ models/Profile.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-
+//reference to users collection and create new collection profile
 const ProfileSchema = new Schema ({
   user: {
     type: Schema.Types.ObjectId,
@@ -24,10 +24,55 @@ const ProfileSchema = new Schema ({
   },
   dateofbirth:{
     type: Date
-  }
-
+  },
+ //schema for followers and following.
+  following: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: "users",
+      },
+      name: {
+        type: String,
+      },
+      avatar: {
+        type: String,
+      },
+      username: {
+        type: String
+      },
+      profile: {
+        type: Schema.Types.ObjectId,
+        ref: "profile",
+      }
+    }
+  ],
+  followers: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: "users",
+      },
+      name: {
+        type: String,
+      },
+      avatar: {
+        type: String,
+      },
+      username: {
+        type: String
+      },
+      profile: {
+        type: Schema.Types.ObjectId,
+        ref: "profile",
+      }
+    }
+  ],
+  date: {
+    type: Date,
+    default: Date.now
+  } 
 });
-
 const Profile = mongoose.model
 ('profile', ProfileSchema);
 module.exports = Profile;
