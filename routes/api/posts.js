e652c6a677585bb4095d768780eb772811b92ee3
Lemeshevskaya <birtochka@gmail.com>
2021-04-02 02:36:01 -0700

    create Post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,8 +1,106 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
 
-//dummy test
-router.get('/test', (req,res) => res.json({msg: 'posts works.'}));
+// Post model
+const Post = require('../../models/Post');
+// Profile model
+const Profile = require('../../models/Profile');
 
-//export
+
+// @route   GET api/posts
+// @desc    Get posts
+// @access  Public
+router.get('/', (req, res) => {
+  Post.find()
+    .sort({ date: -1 })
+    .then(posts => res.json(posts))
+    .catch(err => res.status(404).json({ nopostsfound: 'No posts found' }));
+});
+
+
+// @route   POST api/posts
+// @desc    Create post
+// @access  Private
+router.post(
+  '/',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+
+    const newPost = new Post({
+      user: req.user.id,
+      username: req.body.username,
+      avatar: req.body.avatar,
+      image: req.body.image,
+      text: req.body.text,
+      location: req.body.location,
+      Date: req.body.date
+    });
+
+    newPost.save().then(post => res.json(post));
+  }
+);
+
+
+// @route   POST api/posts/like/:idpost
+// @desc    Like post
+// @access  Private
+router.post(
+  '/like/:id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      Post.findById(req.params.id)
+        .then(post => {
+          if (
+            post.likes.filter(like => like.user.toString() === req.user.id)
+              .length > 0
+          ) {
+            return res
+              .status(400)
+              .json({ alreadyliked: 'User already liked this post' });
+          }
+
+          // Add user id to likes
+          post.likes.unshift({ user: req.user.id });
+
+          post.save().then(post => res.json(post));
+        })
+        .catch(err => res.status(404).json({ postnotfound: 'No post found' }));
+    });
+  }
+);
+
+
+
+// @route   POST api/posts/comment/:idpost
+// @desc    Add comment to post
+// @access  Private
+router.post(
+  '/comment/:id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+
+
+    Post.findById(req.params.id)
+      .then(post => {
+        const newComment = {
+          text: req.body.text,
+          username: req.body.username,
+          avatar: req.body.avatar,
+          user: req.user.id
+        };
+
+        // Add to comments array
+        post.comments.unshift(newComment);
+
+        // Save comments
+        post.save().then(post => res.json(post));
+      })
+      .catch(err => res.status(404).json({ postnotfound: 'No post found' }));
+  }
+);
+
+
 module.exports = router;
