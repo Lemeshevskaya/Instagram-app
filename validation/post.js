220429f19982270a361fd3095f3eb237bcd3c757
Gulshan-p <78453319+Gulshan-p@users.noreply.github.com>
2021-04-04 00:04:30 -0700

    added validations to profile and post

---
+++ validation/post.js
@@ -0,0 +1,22 @@
+const Validator = require('validator');
+const isEmpty = require('./is-empty');
+//add module.export and call the function
+module.exports = function validatePostInput(data){
+    let errors = {};
+//add validations
+//for text field
+if(!Validator.isLength(data.text, {min:5, max:300})){
+  errors.text = 'text must be between 5 and 300 characters';
+   }
+   if(isEmpty(data.text)){
+     errors.text = 'Text field is required';
+   }
+   //for image
+   if (isEmpty(data.image)) {
+    errors.image = "Image is required";
+  }
+   return{
+    errors,
+    isValid: isEmpty(errors)
+  }
+}
