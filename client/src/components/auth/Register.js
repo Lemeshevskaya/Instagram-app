e8dbaf43b3b800177d9f167b4644cb77d55402e8
Gulshan-p <78453319+Gulshan-p@users.noreply.github.com>
2021-04-16 20:14:43 -0700

    added states and end to end testing for Login ,Register

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import {axios} from 'axios';
+import axios from 'axios';
+import classnames from 'classnames';
 
 class Register extends Component {
   constructor(){
@@ -8,6 +9,7 @@ class Register extends Component {
       name: '',
       email: '',
       password: '',
+      password2: '',
       errors: {}
     }
   }
@@ -16,35 +18,71 @@ class Register extends Component {
   }
 
   onSubmit(e) {
-    
+    e.preventDefault()
 
+    const newUser = {
+      name: this.state.name,
+      email: this.state.email,
+      password: this.state.password,
+      password2: this.state.password2,
+    };
+    axios.post('api/users/register', newUser)
+    .then(res => console.log(res.data))
+    .catch(err => this.setState({errors: err.response.data}))
   }
 
   render() {
+    const {errors} = this.state;
     return (
-      <div className="register">
+    <div className="register">
     <div className="container">
       <div className="row">
         <div className="col-md-8 m-auto">
           <h1 className="display-4 text-center">Sign Up</h1>
-          <p className="lead text-center">Create your DevConnector account</p>
-          <form onSubmit={this.onSubmit.bind(this)}>
+          <p className="lead text-center">Create your Instagram Account</p>
+          <form noValidate onSubmit 
+          = {this.onSubmit.bind(this)}>
             <div className="form-group">
-              <input type="text" className="form-control form-control-lg" placeholder="Name" 
+              <input type="text" className={classnames('form-control form-control-lg', {'is-invalid': errors.name})} placeholder="Name" 
               name="name" 
               value = {this.state.name} 
               onChange={this.onChange.bind(this)} required />
+              {errors.name && (
+                <div className = "invalid-feedback">
+                  {errors.name}
+                </div>
+              )}
             </div>
             <div className="form-group">
-              <input type="email" className="form-control form-control-lg" placeholder="Email Address" 
+              <input type="email" className={classnames('form-control form-control-lg', {'is-invalid': errors.email})} placeholder="Email Address" 
               value = {this.state.email} 
               onChange={this.onChange.bind(this)} name="email" />
               <small className="form-text text-muted">This site uses Gravatar so if you want a profile image, use a Gravatar email</small>
+              {errors.email && (
+                <div className = "invalid-feedback">
+                  {errors.email}
+                </div>
+              )}
             </div>
             <div className="form-group">
-              <input type="password" className="form-control form-control-lg" placeholder="Password" 
+              <input type="password" className={classnames('form-control form-control-lg', {'is-invalid': errors.password})} placeholder="Password" 
               value = {this.state.password} 
               onChange={this.onChange.bind(this)} name="password" />
+               {errors.password && (
+                <div className = "invalid-feedback">
+                  {errors.password}
+                </div>
+              )}
+            </div>
+            <div className="form-group">
+              <input type="password" className={classnames('form-control form-control-lg', {'is-invalid': errors.password2})} placeholder="Confirm Password" 
+              value = {this.state.password2} 
+              onChange={this.onChange.bind(this)} name="password2" />
+              {errors.password2 && (
+                <div className = "invalid-feedback">
+                  {errors.password2}
+                </div>
+              )}
             </div>
             <input type="submit" className="btn btn-info btn-block mt-4" />
           </form>
