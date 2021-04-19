ff675d27fd60cede29c2258cbd8d3e1d4ed91873
Gracielagchz <77737186+Gracielagchz@users.noreply.github.com>
2021-04-19 07:05:20 -0700

    Reducer

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {Provider} from 'react-redux';
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Footer from './components/layout/Footer';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
+import store from './store';
 
 export default class App extends Component {
   render() {
     return (
+      <Provider store={}>
       <Router>
         <div className="App">
           <Navbar />
@@ -19,6 +22,7 @@ export default class App extends Component {
           <Footer />
         </div>
       </Router>
+      </Provider>
     )
   }
 }
