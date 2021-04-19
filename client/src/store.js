0b157c519b0ba1de9f931eb7e76bdb4a375bf364
Gracielagchz <77737186+Gracielagchz@users.noreply.github.com>
2021-04-19 15:25:59 -0700

    Fixed brackets

--- client/src/store.js
+++ client/src/store.js
@@ -2,7 +2,7 @@ import {createStore, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers/index'
 
-const middleware = {thunk};
+const middleware = [thunk];
 
 const store = createStore(
   rootReducer,
