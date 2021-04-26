1bbe3def24ea81c0a0101a3bfd6162233861cc4f
Gracielagchz <77737186+Gracielagchz@users.noreply.github.com>
2021-04-26 03:52:14 -0700

    complete UI code

--- client/src/App.js
+++ client/src/App.js
@@ -1,58 +1,111 @@
 import React, { Component } from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
 import {Provider} from 'react-redux';
 import jwt_decode from 'jwt-decode';
-import './App.css';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Footer from './components/layout/Footer';
-import Login from './components/auth/Login';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import './App.css';
 import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 import store from './store';
 import { logoutUser } from './actions/authActions';
 import setAuthToken from './utils/setAuthToken';
-import { SET_CURRENT_USER } from './actions/types';
+import {SET_USER} from './actions/types';
 
-//check for token in local storage
-if(localStorage.jwtToken){
-  //if found
-  //decode token
+import PrivateRoute from "./components/common/PrivateRoute";
+
+import { clearCurrentProfile } from "./actions/profileActions";
+import Dashboard from "./components/dashboard/Dashboard";
+import CreateProfile from "./components/create-profile/CreateProfile";
+import EditProfile from "./components/edit-profile/EditProfile";
+import AddExperience from "./components/add-credentials/AddExperience";
+import AddEducation from "./components/add-credentials/AddEducation";
+import Profiles from "./components/profiles/Profiles";
+import Profile from "./components/profile/Profile";
+import Posts from "./components/posts/Posts";
+import Post from "./components/post/Post";
+import NotFound from "./components/not-found/NotFound";
+
+if (localStorage.jwtToken){
+  //decode
   const decoded = jwt_decode(localStorage.jwtToken);
-  //check for expired token
+  //check the expiry of the token
   const currentTime = Date.now()/1000;
-  //if token is expired
-  if(decoded.exp < currentTime){
-
-    //then logout user automatically
+  if (decoded.exp < currentTime){
+    //Expired
+    //Logout user
     store.dispatch(logoutUser());
-    //redirect user to login page
+    //Redirect user to login
     window.location.href = "/login";
   }
-  //else if not expired
-    //set auth header
-     setAuthToken(localStorage.jwtToken);
-    //SET_CURRENT_USER
-      store.dispatch({
-       type: SET_CURRENT_USER,
-        payload: decoded
-      });
-}
 
-export default class App extends Component {
+  //Set auth header
+  setAuthToken(localStorage.jwtToken);
+  //dispatch
+  store.dispatch({
+    type: SET_USER,
+    payload: decoded,
+  });
+}
+class App extends Component {
   render() {
     return (
-      <Provider store= {store}>
-      <Router>
-        <div className="App">
-          <Navbar />
-          <Route exact path = "/" component={Landing} />
-          <Route exact path= "/register" component ={Register}/>
-          <Route exact path="/login" component={Login} />
-          <Footer />
-        </div>
-      </Router>
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <Route exact path="/" component={Landing} />
+            <div className="container">
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/profiles" component={Profiles} />
+              <Route exact path="/profile/:handle" component={Profile} />
+              <Switch>
+                <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              </Switch>
+              <Switch>
+                <PrivateRoute
+                  exact
+                  path="/create-profile"
+                  component={CreateProfile}
+                />
+              </Switch>
+              <Switch>
+                <PrivateRoute
+                  exact
+                  path="/edit-profile"
+                  component={EditProfile}
+                />
+              </Switch>
+              <Switch>
+                <PrivateRoute
+                  exact
+                  path="/add-experience"
+                  component={AddExperience}
+                />
+              </Switch>
+              <Switch>
+                <PrivateRoute
+                  exact
+                  path="/add-education"
+                  component={AddEducation}
+                />
+              </Switch>
+              <Switch>
+                <PrivateRoute exact path="/feed" component={Posts} />
+              </Switch>
+              <Switch>
+                <PrivateRoute exact path="/post/:id" component={Post} />
+              </Switch>
+              <Route exact path="/not-found" component={NotFound} />
+            </div>
+            <Footer />
+          </div>
+        </Router>
       </Provider>
-    )
+    );
   }
 }
 
+export default App;
--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,20 +1,21 @@
-import {SET_CURRENT_USER} from '../actions/types';
-import isEmpty from '../validation/is-empty';
+import isEmpty from "../validation/is-empty";
+import { SET_USER } from "../actions/types";
 
 const initialState = {
   isAuthenticated: false,
   user: {}
 };
 
-export default function (state = initialState, action){
+export default function(state=initialState, action){
+
   switch(action.type){
-    case SET_CURRENT_USER:
+    case SET_USER:
       return {
         ...state,
         isAuthenticated: !isEmpty(action.payload),
         user: action.payload
       }
     default:
-      return state;
+      return state
   }
 }
