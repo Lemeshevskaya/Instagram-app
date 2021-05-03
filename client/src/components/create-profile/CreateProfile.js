b950b7b1a2791cd9d5e45bc28f4d843eb8457282
Lemeshevskaya <birtochka@gmail.com>
2021-05-02 17:29:17 -0700

    change footer

--- client/src/components/create-profile/CreateProfile.js
+++ client/src/components/create-profile/CreateProfile.js
@@ -64,7 +64,7 @@ class CreateProfile extends Component {
 
     return (
       <div className="create-profile">
-        <div className="container">
+        <div className="containerm container_profile">
           <div className="row">
             <div className="col-md-8 m-auto">
               <h1 className="display-4 text-center">Create Your Profile</h1>
